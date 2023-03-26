c05f66622a3e4660990f56ef24c0670d838e4d89
Sergey Vaselenko <s.vaselenko@gartle.com>
2023-03-26 22:32:16 +0300

    version 10.8

--- sources/savetodb-framework-list.sql
+++ sources/savetodb-framework-list.sql
@@ -1,8 +1,8 @@
 -- =============================================
 -- SaveToDB Framework for NuoDB
--- Version 10.6, December 13, 2022
+-- Version 10.8, January 9, 2023
 --
--- Copyright 2014-2022 Gartle LLC
+-- Copyright 2014-2023 Gartle LLC
 --
 -- License: MIT
 -- =============================================
